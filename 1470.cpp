c0cb4ec0b82af861baab55ffa344b09c4cde0907
HA <hoanganh.theodore@icloud.com>
2021-05-01 00:57:28 -0400

    added 1470

---
+++ 1470.cpp
@@ -0,0 +1,19 @@
+#include "gen.h"
+
+// https://leetcode.com/problems/shuffle-the-array
+
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) {
+        int temp;
+        int k = n;
+        for (size_t i = 1; i <= n*2; i+=2)
+        {
+            temp = nums[k];
+            nums.erase(nums.begin() + k);
+            nums.insert(nums.begin() + i, temp);
+            k++;
+        }
+        return nums;
+    }
+};
