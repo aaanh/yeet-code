aa368b5610897dcb5284719c2a0b8080a05e1aec
Anh <iam@hoanganh.tech>
2022-02-10 22:38:03 -0500

    rename and added mid/704

---
+++ mid/704-binary-search.cpp
@@ -0,0 +1,34 @@
+#include "../gen.h"
+
+// https://leetcode.com/problems/binary-search/
+// Runtime	: 0 ms
+// Memory	: 6.1 MB
+
+// ! Solution code was taken from Leet Code
+// Disclaimer: Created for the purpose of code and problem comprehension.
+
+class Solution 
+{
+    public:
+        int search(vector<int>& nums, int target) {
+        int pivot, left = 0, right = nums.size() - 1;
+        while (left <= right) 
+        {
+            pivot = left + (right - left) / 2;
+            if (nums[pivot] == target) return pivot;
+            if (target < nums[pivot]) right = pivot - 1;
+            else left = pivot + 1;
+        }
+        return -1;
+    }
+};
+
+int main() 
+{
+    Solution solution;
+    vector<int> input = {-1,0,3,5,9,12};
+    int index = solution.search(input, 9);
+    printf("Index is: %d", index);
+    return index;
+    
+}
---
+++ problem-template.cpp
@@ -0,0 +1,5 @@
+#include "../gen.h"
+
+// Link     : 
+// Runtime	: 0 ms
+// Memory	: 6.1 MB
